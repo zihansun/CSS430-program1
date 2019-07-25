f2667a4ee1fdc9d6206b393658e3983470627798
zihansun <34561248+zihansun@users.noreply.github.com>
2019-07-25 16:57:44 -0700

    Update processes.cpp

--- processes.cpp
+++ processes.cpp
@@ -1,9 +1,6 @@
 //
 //  processes.cpp
 //  CSS430
-//
-
-
 #include <stdio.h>
 #include <stdlib.h>     //exit
 #include <unistd.h>     //fork, pipe
@@ -16,6 +13,12 @@ int main(int argc, char** argv)
     enum {READ, WRITE};
     pid_t pid1, pid2, pid3;
     int pipeFD1[2],pipeFD2[2];
+
+    // check if pipes are created ok
+    if(pipe(pipeFD1)<0 || pipe(pipeFD2)<0){
+        perror("Error in creating pipe");
+        exit(EXIT_FAILURE);
+    }
     
     //check if one argument is provided
     if(argc < 2)
@@ -23,25 +26,13 @@ int main(int argc, char** argv)
         perror("Error in Argument: required 1");
         exit(EXIT_FAILURE);
     }
-    
-    //check if the pipe is correctly created
-    if (pipe(pipeFD1) < 0)
-    {
-        perror("Error in creating pipe 1");
-        exit(EXIT_FAILURE);
-    }
-    if (pipe(pipeFD2) < 0)
-    {
-        perror("Error in creating pipe 2");
-        exit(EXIT_FAILURE);
-    }
-    
+
     //creating the fork process
     pid1 = fork();
     //check if the fold process is good
     if (pid1 < 0)
     {
-        perror("Error during the first fork");
+        perror("Error during fork");
         exit(EXIT_FAILURE);
     }
     
@@ -51,7 +42,7 @@ int main(int argc, char** argv)
         pid2 = fork();
         if (pid2 < 0)
         {
-            perror("Error during the second fork");
+            perror("Error during fork");
             exit(EXIT_FAILURE);
         }
         else if (pid2 == 0)//Child2
@@ -70,7 +61,11 @@ int main(int argc, char** argv)
                 close(pipeFD2[WRITE]);
                 close(pipeFD1[READ]);
                 dup2(pipeFD2[READ], 0);
-                execlp("wc", "wc", "-l", NULL);
+                int check = execlp("wc", "wc", "-l", NULL);
+                if(check == -1){
+                perror("Error on executing command");
+                exit(EXIT_FAILURE);
+            }
             }
             else
             {
@@ -79,7 +74,11 @@ int main(int argc, char** argv)
                 dup2(pipeFD2[WRITE],1);
                 dup2(pipeFD1[READ],0);
                 close(pipeFD2[READ]);
-                execlp("grep", "grep", argv[1], NULL);
+                int check = execlp("grep", "grep", argv[1], NULL);
+                if(check == -1){
+                perror("Error on executing command");
+                exit(EXIT_FAILURE);
+            }
             }
         }
         else
@@ -89,7 +88,11 @@ int main(int argc, char** argv)
         close(pipeFD2[WRITE]);
         close(pipeFD1[READ]);
         close(pipeFD2[READ]);
-        execlp("ps", "ps", "-A", NULL);
+        int check = execlp("ps", "ps", "-A", NULL);
+        if(check == -1){
+                perror("Error on executing command");
+                exit(EXIT_FAILURE);
+            }
         }
     }
     else
